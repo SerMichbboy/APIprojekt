ffc9a93f35f5a7a551c3af6c39130262436b834d
Sergo <Sergofiles-16@mail.ru>
2023-05-03 20:56:37 +0300

    comm

--- ETHUSDT.py
+++ ETHUSDT.py
@@ -15,28 +15,29 @@ list_of_value = []
 
 
 # creating a list len < 60
-def add_to_list(vle):
-    if len(list_of_value) >= 60:
+def add_to_list(vle, rfshtime):
+    if len(list_of_value) >= 60 // rfshtime:
         list_of_value.pop(0)
     list_of_value.append(vle)
 
 
 # creating the function calculating the price change
-def calc_price(old_price, now_price):
-    for i in range(old_price):
-        return abs(now_price - old_price[i]) / old_price[i] * 100
+def calc_price(old_prices, now_price):
+    for i in range(old_prices):
+        return abs(now_price - old_prices[i]) / old_prices[i] * 100
 
 
+refresh_time = int(input('частота обновления данных(сек): '))
+check_count = 0
 # start an infinity cycle of checking incoming data
 while True:
-    check_count = 0
-    refresh_time = int(input('частота обновления данных(сек): '))
+    check_count += 1
     new_price = requests.get(url, headers=headers).json()
     # creating a condition if the price change in the last hour is more than 1% - we display a message:
     if calc_price(list_of_value, new_price['rate']) >= 1:
         print('Цена ETH/USDT изменилась на 1% за последние 60 минут')
     elif check_count >= 60 / refresh_time:
-        add_to_list(new_price)
+        add_to_list(new_price, refresh_time)
         check_count = 0
     # frequency of data output
     time.sleep(refresh_time)
